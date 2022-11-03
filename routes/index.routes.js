88c3bff6342d20bfdafb251063aad9105a608309
Oscar Alva <oscaraldrin23@gmail.com>
2022-11-03 15:42:35 +0100

    v2

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,42 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User.model");
+const Anime = require("../models/Anime.model");
+const Episode = require("../models/Episode.model");
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
+
+// router.post("/uploadVideo/:userId", (req, res, next) => {
+//   Episode.create({
+//     name: req.body.name,
+//     number: req.body.number,
+//     episodeImg: req.body.episodeImg,
+//   })
+//     .then((episode) => {
+//       return User.findByIdAndUpdate(
+//         req.params.userId,
+//         { $push: { episodes: episode._id } },
+//         { new: true }
+//       );
+//     })
+// })
+
+router.post("/uploadVideo/:userId", (req, res, next) => {
+  Episode.create({
+    name: "naruto",
+    number: 12,
+    episodeImg: "episodio 12",
+    isPremium: false,
+  })
+    .then((episode) => {
+      return User.findByIdAndUpdate(
+        req.params.userId,
+        { $push: { episodes: episode._id } },
+        { new: true }
+      );
+    })
+})
 
 module.exports = router;
