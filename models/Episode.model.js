b691cf37146523348a6b13a8a45d216ed76fbc6c
Jordi Martinez <jordiupc87@gmail.com>
2022-11-03 15:40:07 +0100

    API services working without functionality

--- models/Episode.model.js
+++ models/Episode.model.js
@@ -4,28 +4,30 @@ const episodeSchema = new Schema(
   {
     name: {
       type: String,
-      
     },
     number: {
       type: Number,
       required: true,
-      
     },
-   
+
     episodeImg: {
       type: String,
       default: "/images/profileuser.png",
     },
-    isPremium:{
-        type: Boolean,
-        default: false,
-        require: true,
-
+    isPremium: {
+      type: Boolean,
+      default: false,
+      require: true,
     },
+    episodeUrl: {
+      type: String,
+      require: true,
+    },
+
     animeTitle: { type: Schema.Types.ObjectId, ref: "Anime" }, //Si vols treure el nom del anime fes servir populate.
     likeUsers: [{ type: Schema.Types.ObjectId, ref: "User" }],
   },
-  
+
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
     timestamps: true,
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,45 @@
 const express = require("express");
 const router = express.Router();
+const AnimeModel = require("../models/Anime.model");
+const EpisodeModel = require("../models/Episode.model");
 
-router.get("/", (req, res, next) => {
-  console.log("HELLO");
-  res.json("All good in here");
+router.get("/animes", (req, res, next) => {
+  console.log("THIS IS BACK" + "/" + " ROUTE'S RESPONSE");
+  // res.json("All good in here");
+});
+
+router.get("/animes/:animeId", (req, res, next) => {
+  console.log("this is : ROUTE GET /:animeId");
+});
+router.post("/animes", (req, res, next) => {
+  console.log("this is add new anime: ROUTE POST /");
+});
+router.put("animes/:animeId", (req, res, next) => {
+  console.log("this is EDIT: ROUTE PUT /:animeId");
+});
+router.delete("animes/:animeId", (req, res, next) => {
+  console.log("this is DELETe : ROUTE DEL /:animeId");
+});
+
+// ------- Episodes
+
+router.get("/episodes", (req, res, next) => {
+  console.log("THIS IS BACK, GET EPISODES FULL LIST");
+  // res.json("All good in here");
+  next();
+});
+
+router.get("/episodes/:episodeId", (req, res, next) => {
+  console.log("this is : ROUTE GET /:episodeId");
+});
+router.post("/episodes", (req, res, next) => {
+  console.log("this is add new episode: ROUTE POST /");
+});
+router.put("episodes/:episodeId", (req, res, next) => {
+  console.log("this is EDIT: ROUTE PUT /:episodeId");
+});
+router.delete("episodes/:episodeId", (req, res, next) => {
+  console.log("this is DELETe : ROUTE DEL /:episodeId");
 });
 
 module.exports = router;
